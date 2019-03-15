f72ba0dfbb9527f7ebe4959cbe54b8bfcf5c9b0a
yangfei <yangfei@threathunter.cn>
2019-03-15 09:38:23 +0800

    3

--- src/main.js
+++ src/main.js
@@ -3,9 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VeeValidate from 'vee-validate';
-
+import VeeValidate,{ Validator } from 'vee-validate';
+// import VueI18n from 'vue-i18n';
+import validationMessages from 'vee-validate/dist/locale/zh_CN';
 Vue.use(VeeValidate);
+
+// Localize takes the locale object as the second argument (optional) and merges it.
+Validator.localize('ar', validationMessages );
+// Vue.use(VueI18n);
+
+// const i18n = new VueI18n({locale: 'zh_CN',});
+Vue.use(VeeValidate, {
+  // i18nRootKey: 'validations', // customize the root path for validation messages.
+  // i18n,
+  // dictionary: {
+  //   en: validationMessages
+  // }
+});
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
